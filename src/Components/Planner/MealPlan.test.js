36d087e3a202debe68c3e6bd9005ce8abf90fde0
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-02-26 11:59:12 -0700

    added snapshots to mealplan and makemealplan

--- src/Components/Planner/MealPlan.test.js
+++ src/Components/Planner/MealPlan.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MealPlan from "./MealPlan";
+import { BrowserRouter } from "react-router-dom";
+import renderer from "react-test-renderer";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -9,4 +11,14 @@ it("renders without crashing", () => {
     '<div id="individual-mealplan-view"><section id="original-mealplan-data"><h2 id="update-header">Update</h2><p>Title: <br>Planned date: <br>Time to make: <br>Ingredients required:</p></section><button id="update-mealplan-button" type="submit">Update</button><button id="delete-mealplan-button" type="submit">Delete</button><div></div><button id="go-back-button" type="submit">Go back</button></div>'
   );
   ReactDOM.unmountComponentAtNode(div);
+});
+it("renders the UI as expected", () => {
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <MealPlan />
+      </BrowserRouter>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
