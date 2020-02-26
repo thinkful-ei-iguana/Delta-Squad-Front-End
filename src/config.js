8362a33410fd100e3372b34c580bc3d21cf99412
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-02-26 12:57:16 -0700

    should be a working front end

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export default {
   API_ENDPOINT: "https://sheltered-waters-10877.herokuapp.com/api",
 
   // API_ENDPOINT: "http://localhost:8000/api",
-
+  //
   REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
 };
